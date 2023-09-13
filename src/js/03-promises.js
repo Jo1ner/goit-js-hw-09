beb911d173984d7c1af883dfa0a88ef97c14f59c
Jo1ner <semdimas@gmail.com>
2023-09-13 10:59:36 +0300

    fix task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -9,17 +9,22 @@ function onSubmit(e) {
   const firstDelay = Number(form.elements.delay.value);
   const delayStep = Number(form.elements.step.value);
   const amount = Number(form.elements.amount.value);
-  
-  for (let i = 1; i <= amount; i += 1) {
-      let delay = firstDelay + delayStep * (i - 1);
 
-      createPromise(i, delay)
+  if (firstDelay < 0 || delayStep < 0 || amount <= 0) {
+    Notiflix.Notify.failure('❌ Please enter positive value')
+    return;
+  }
+
+  let delay = firstDelay;
+  for (let i = 1; i <= amount; i += 1) {
+         createPromise(i, delay)
         .then(({ position, delay }) => {
           Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
         })
         .catch(({ position, delay }) => {
           Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
         });
+    delay += delayStep;
     };
 }; 
 
