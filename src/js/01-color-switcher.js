c5bf3acf5290c657ff52794f153e31713faa755b
Jo1ner <semdimas@gmail.com>
2023-09-13 10:17:23 +0300

    fix task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,6 +1,7 @@
 
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+let timerId = null;
 
 btnStart.addEventListener('click', onStart); 
 function onStart() {
